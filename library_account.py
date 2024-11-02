9289a3b2777c5670da8c3c110e5b8203d0b1148d
faruklmu17 <41293321+faruklmu17@users.noreply.github.com>
2024-11-02 10:58:09 -0400

    Update library_account.py

--- library_account.py
+++ library_account.py
@@ -10,3 +10,12 @@ available_books = [
 "Divergent", "Eleanor & Park"
 
 ]
+class LibraryAccount:
+    
+    def __init__(self,user_name,borrowed_books= None,available_books):
+        self.user_name = user_name
+        self.available_books = available_books
+        if borrowed_books is not None:
+            self.borrowed_books = borrowed_books
+        else:
+            self.borrowed_books = borrowed_books
