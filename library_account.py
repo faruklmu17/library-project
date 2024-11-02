c1645a4aae53dc5fed8395b5bd32e97d97062fae
faruklmu17 <41293321+faruklmu17@users.noreply.github.com>
2024-11-02 10:32:40 -0400

    Create library_account.py

---
+++ library_account.py
@@ -0,0 +1,12 @@
+# a library has the folowing books available
+available_books = [
+
+"The Fault in Our Stars", "The Hunger Games", "To All the Boys I've Loved Before",
+
+"Harry Potter and the Sorcerer's Stone", "The Perks of Being a Wallflower",
+
+"Looking for Alaska", "The Hate U Give", "The Maze Runner",
+
+"Divergent", "Eleanor & Park"
+
+]
